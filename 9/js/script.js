b8ecfae64682e29de405797c4160f9e78cef9684
Igor <example@gmail.com>
2021-11-26 12:09:05 +0500

    Свойства и методы

---
+++ 9/js/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// const str = 'text';
+// const arr = [1, 2, 3];
+// Свойство length
+/* console.log(str.length);
+console.log(arr.length); */
+
+/* console.log(str[2]); */
+
+// Методы строк
+
+/* console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str); */
+
+/* const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('g'));
+console.log(fruit.indexOf('fruit')); */
+
+
+//const logg = 'Hello World';
+
+/* // console.log(logg.slice(6, 11));
+// console.log(logg.slice(4));
+//console.log(logg.slice(-5, -2)); */
+
+/* console.log(logg.substring(6, 11)); */
+
+/* console.log(logg.substr(1, 7));
+console.log(logg.substr(1, 2));
+console.log(logg.substr(2, 5)); */
+
+
+// Методы чисел
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
