c95ab33c4f319c882c7a4c21251dbe964dbc7e55
Igor <example@gmail.com>
2021-11-28 19:41:54 +0500

    JS - основы

---
+++ JS основы/12/js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+//const arr = [1, 2, 3, 6, 8];
+/* // arr.pop();
+arr.push(10);
+console.log(arr); */
+
+/* for (let i = 0; i < arr.length; i++) {
+	console.log(arr[i]);
+} */
+
+/* for (value of arr) {
+	console.log(value);
+} */
+
+/* arr[99] = 0;
+console.log(arr.length);
+console.log(arr); */
+
+
+/* arr.forEach(function (item, i, arr) {
+	console.log(`${i}: ${item} внутри массива ${arr}`);
+}); */
+
+/* const str = prompt("", "");
+const products = str.split(", ");
+//console.log(products);
+products.sort();
+console.log(products.join('; ')); */
+
+const arr = [20, 10, 13, 65, 55, 8];
+/* arr.sort();
+console.log(arr); */
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b) {
+	return (a - b);
+}
